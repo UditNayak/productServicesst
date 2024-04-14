b42f6f1d9dc86277ad98ab6bf17ef8c79f220f87
UditNayak <uditnayak027@gmail.com>
2024-04-14 16:59:00 +0530

    Update Readme of DataBase step

--- src/main/java/com/udit/productservicesst/models/Product.java
+++ src/main/java/com/udit/productservicesst/models/Product.java
@@ -1,5 +1,8 @@
 package com.udit.productservicesst.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,11 +12,14 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor  // All Arguments Constructor
 @NoArgsConstructor   // No Argument Constructor ( Default Constructor )
+@Entity
 public class Product {
+    @Id
     private Long id;
     private String title;
     private String description;
     private double price;
+    @ManyToOne
     private Category category;
     private String image;
 }
